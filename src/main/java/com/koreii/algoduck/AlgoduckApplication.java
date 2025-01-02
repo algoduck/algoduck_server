3453eee4db663abf466ed65b4418360739e6f496
jwelyl <cork2586@naver.com>
2025-01-02 20:31:38 +0900

    initial commit

---
+++ src/main/java/com/koreii/algoduck/AlgoduckApplication.java
@@ -0,0 +1,13 @@
+package com.koreii.algoduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoduckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoduckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koreii/algoduck/AlgoduckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreii.algoduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoduckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
