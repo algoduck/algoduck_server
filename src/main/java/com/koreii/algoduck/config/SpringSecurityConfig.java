1d2bc770a30044e2776c9c3ba71f5c4008747007
jwelyl <cork2586@naver.com>
2025-01-27 23:25:55 +0900

    [refactor] BCrypPasswordEncoder Spring Bean 등록

--- src/main/java/com/koreii/algoduck/config/SpringSecurityConfig.java
+++ src/main/java/com/koreii/algoduck/config/SpringSecurityConfig.java
@@ -5,10 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf(AbstractHttpConfigurer::disable)
