9ae122c9ae8d8f20f96632e50472071e8ef1bfb8
jwelyl <cork2586@naver.com>
2025-02-10 15:21:39 +0900

    [refactor] FileStorage DIP 적용

--- src/main/java/com/koreii/algoduck/file/FileStorageService.java
+++ src/main/java/com/koreii/algoduck/file/FileStorageService.java
@@ -3,7 +3,7 @@ package com.koreii.algoduck.file;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileStorageService {
-  String uploadFile(String bucketName, String filePath, MultipartFile file);
+  String uploadFile(String repositoryName, String folderPath, MultipartFile file);
 
-  void deleteFile(String bucketName, String filePath);
+  void deleteFile(String repositoryName, String fileUrl);
 }
