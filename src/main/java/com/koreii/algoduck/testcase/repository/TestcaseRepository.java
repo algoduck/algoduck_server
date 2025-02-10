75ab04fbf20218728a2bb5e4c65064b7b4232cc3
jwelyl <cork2586@naver.com>
2025-02-10 17:19:15 +0900

    [feat] Testcase Repository, Service 구현

--- src/main/java/com/koreii/algoduck/testcase/repository/TestcaseRepository.java
+++ src/main/java/com/koreii/algoduck/testcase/repository/TestcaseRepository.java
@@ -3,10 +3,17 @@ package com.koreii.algoduck.testcase.repository;
 import com.koreii.algoduck.problem.entity.Problem;
 import com.koreii.algoduck.testcase.dto.request.TestcaseAddRequestDto;
 import com.koreii.algoduck.testcase.dto.response.TestcaseResponseDto;
+import com.koreii.algoduck.testcase.entitiy.Testcase;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TestcaseRepository {
   //  TestcaseService에서 테스트케이스가 저장소에 제대로 저장된 다음에 해당 경로를 이용해 DB에 저장
   TestcaseResponseDto addTestcase(Problem problem, TestcaseAddRequestDto testcaseAddRequestDto);
+
+  Testcase findByTestcaseId(Long testcaseId);
+
+  List<TestcaseResponseDto> selectTestcasesByProblemId(Long problemId);
 }
--- src/main/java/com/koreii/algoduck/testcase/service/TestcaseServiceImpl.java
+++ src/main/java/com/koreii/algoduck/testcase/service/TestcaseServiceImpl.java
@@ -1,18 +1,25 @@
 package com.koreii.algoduck.testcase.service;
 
+import com.koreii.algoduck.exceptions.file.FileUploadFailException;
 import com.koreii.algoduck.file.FileStorageService;
 import com.koreii.algoduck.problem.dto.response.ProblemResponseDto;
 import com.koreii.algoduck.problem.entity.Problem;
 import com.koreii.algoduck.problem.repository.ProblemRepository;
+import com.koreii.algoduck.testcase.dto.request.TestcaseAddRequestDto;
 import com.koreii.algoduck.testcase.dto.response.TestcaseResponseDto;
 import com.koreii.algoduck.testcase.repository.TestcaseRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
+@Slf4j
 public class TestcaseServiceImpl implements TestcaseService {
   private final TestcaseRepository testcaseRepository;
   private final ProblemRepository problemRepository;
@@ -20,12 +27,43 @@ public class TestcaseServiceImpl implements TestcaseService {
 
   @Value("${spring.cloud.aws.s3.testcase_bucket}")
   private String bucketName;
-  
+
   @Override
+  @Transactional
   public TestcaseResponseDto addTestcase(Long problemId, MultipartFile testcaseInput, MultipartFile testcaseOutput, boolean isPublic) {
     Problem problem = problemRepository.findByProblemId(problemId);
+    String testcaseInputName = testcaseInput.getName();
+    String testcaseOutputName = testcaseOutput.getName();
 
+    String testcaseInputUrl = null;
+    String testcaseOutputUrl = null;
 
-    return null;
+    try {
+      testcaseInputUrl = fileStorageService.uploadFile(bucketName, "testcase/" + problem.getProblemNumber() + "/input", testcaseInput);
+      testcaseOutputUrl = fileStorageService.uploadFile(bucketName, "testcase/" + problem.getProblemNumber() + "/output", testcaseOutput);
+    } catch (FileUploadFailException e) {
+      log.error("Fail to upload testcase file", e);
+      throw e;
+    }
+
+    try {
+      TestcaseAddRequestDto testcaseAddRequestDto = TestcaseAddRequestDto.builder()
+          .testcaseInputName(testcaseInputName)
+          .testcaseInputUrl(testcaseInputUrl)
+          .testcaseOutputName(testcaseOutputName)
+          .testcaseOutputUrl(testcaseOutputUrl)
+          .isPublic(isPublic)
+          .build();
+
+      return testcaseRepository.addTestcase(problem, testcaseAddRequestDto);
+    } catch (Exception e) {
+      log.error("testcase DB insert fail", e);
+      throw e;
+    }
+  }
+
+  @Override
+  public List<TestcaseResponseDto> selectTestcasesByProblemId(Long problemId) {
+    return testcaseRepository.selectTestcasesByProblemId(problemId);
   }
 }
