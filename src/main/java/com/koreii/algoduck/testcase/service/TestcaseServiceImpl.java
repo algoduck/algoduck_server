6d502f7ecef5085d6687b6163378e413a733c763
jwelyl <cork2586@naver.com>
2025-02-11 23:23:47 +0900

    [feat] testcase DB 저장 실패 시 저장되었던 파일 저장소에서 제거

--- src/main/java/com/koreii/algoduck/testcase/service/TestcaseServiceImpl.java
+++ src/main/java/com/koreii/algoduck/testcase/service/TestcaseServiceImpl.java
@@ -1,21 +1,22 @@
 package com.koreii.algoduck.testcase.service;
 
-import com.koreii.algoduck.exceptions.file.FileUploadFailException;
-import com.koreii.algoduck.file.FileStorageService;
-import com.koreii.algoduck.problem.dto.response.ProblemResponseDto;
-import com.koreii.algoduck.problem.entity.Problem;
-import com.koreii.algoduck.problem.repository.ProblemRepository;
-import com.koreii.algoduck.testcase.dto.request.TestcaseAddRequestDto;
-import com.koreii.algoduck.testcase.dto.response.TestcaseResponseDto;
-import com.koreii.algoduck.testcase.repository.TestcaseRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import com.koreii.algoduck.exceptions.file.FileUploadFailException;
+import com.koreii.algoduck.file.FileStorageService;
+import com.koreii.algoduck.problem.entity.Problem;
+import com.koreii.algoduck.problem.repository.ProblemRepository;
+import com.koreii.algoduck.testcase.dto.request.TestcaseAddRequestDto;
+import com.koreii.algoduck.testcase.dto.response.TestcaseResponseDto;
+import com.koreii.algoduck.testcase.repository.TestcaseRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Repository
 @RequiredArgsConstructor
@@ -58,6 +59,8 @@ public class TestcaseServiceImpl implements TestcaseService {
       return testcaseRepository.addTestcase(problem, testcaseAddRequestDto);
     } catch (Exception e) {
       log.error("testcase DB insert fail", e);
+      fileStorageService.deleteFile(bucketName, testcaseInputUrl);
+      fileStorageService.deleteFile(bucketName, testcaseOutputUrl);
       throw e;
     }
   }
