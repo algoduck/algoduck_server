7536d566a0453a26eb738d4c68d22049961a01de
jwelyl <cork2586@naver.com>
2025-02-14 15:35:56 +0900

    [feat] ProblemRepository 동적 쿼리를 제외한 select 쿼리 구현, OFFSET 기반 paging 구현

--- src/main/java/com/koreii/algoduck/problem/repository/ProblemRepository.java
+++ src/main/java/com/koreii/algoduck/problem/repository/ProblemRepository.java
@@ -2,14 +2,41 @@ package com.koreii.algoduck.problem.repository;
 
 import com.koreii.algoduck.problem.dto.request.ProblemAddRequestDto;
 import com.koreii.algoduck.problem.dto.response.ProblemResponseDto;
+import com.koreii.algoduck.problem.dto.response.ProblemSimpleResponseDto;
 import com.koreii.algoduck.problem.entity.Problem;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProblemRepository {
-  //  문제를 추가함
+  //  문제 이미지, 테스트케이스가 존재할 경우, 이미지와 테스트케이스가 정상적으로 저장된 다음에 문제를 저장함
   ProblemResponseDto addProblem(ProblemAddRequestDto problemAddRequestDto);
 
   //  PK로 문제를 찾음
   Problem findByProblemId(Long problemId);
+
+  //  모든 문제 수를 가져옴
+  long countAllProblems();
+
+  //  모든 문제를 가져옴
+  List<ProblemSimpleResponseDto> selectAllProblems(int pageNumber, int pageSize);
+
+  //  문제 번호가 포함된 문제 수를 가져옴
+  long countProblemsWithProblemNumber(String problemNumber);
+
+  //  문제 번호가 포함된 문제를 찾음
+  List<ProblemSimpleResponseDto> selectProblemsWithProblemNumber(String problemNumber, int pageNumber, int pageSize);
+
+  //  문제 제목을 포함하는 문제 수를 가져옴
+  long countProblemsWithTitle(String title);
+
+  //  문제 제목을 포함하는 문제를 찾음
+  List<ProblemSimpleResponseDto> selectProblemsWithTitle(String title, int pageNumber, int pageSize);
+
+  //  주어진 난이도에 해당하는 문제 수를 가져옴
+  long countProblemsWithDifficulty(List<Integer> difficulties);
+
+  //  주어진 난이도에 해당하는 문제를 찾음
+  List<ProblemSimpleResponseDto> selectProblemsWithDifficulty(List<Integer> difficulties, int pageNumber, int pageSize);
 }
--- src/main/java/com/koreii/algoduck/problem/repository/ProblemRepositoryJpaImpl.java
+++ src/main/java/com/koreii/algoduck/problem/repository/ProblemRepositoryJpaImpl.java
@@ -2,10 +2,13 @@ package com.koreii.algoduck.problem.repository;
 
 import com.koreii.algoduck.problem.dto.request.ProblemAddRequestDto;
 import com.koreii.algoduck.problem.dto.response.ProblemResponseDto;
+import com.koreii.algoduck.problem.dto.response.ProblemSimpleResponseDto;
 import com.koreii.algoduck.problem.entity.Problem;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -30,5 +33,112 @@ public class ProblemRepositoryJpaImpl implements ProblemRepository {
   @Override
   public Problem findByProblemId(Long problemId) {
     return entityManager.find(Problem.class, problemId);
+  }
+
+  @Override
+  public long countAllProblems() {
+    String jpql = "SELECT COUNT(p) FROM Problem p";
+    return entityManager.createQuery(jpql, Long.class).getSingleResult();
+  }
+
+  @Override
+  public List<ProblemSimpleResponseDto> selectAllProblems(int pageNumber, int pageSize) {
+    int offset = (pageNumber - 1) * pageSize;
+
+    String jpql = "SELECT new com.koreii.algoduck.problem.dto.response.ProblemSimpleResponseDto(p) "
+        + "FROM Problem p "
+        + "ORDER BY p.problemNumber";
+
+    return entityManager.createQuery(jpql, ProblemSimpleResponseDto.class)
+        .setFirstResult(offset)
+        .setMaxResults(pageSize)
+        .getResultList();
+  }
+
+  @Override
+  public long countProblemsWithProblemNumber(String problemNumber) {
+    String jpql = "SELECT COUNT(p) "
+        + "FROM Problem p "
+        + "WHERE p.problemNumber LIKE :problemNumber ";
+
+    return entityManager.createQuery(jpql, Long.class).setParameter("problemNumber", "%" + problemNumber + "%")
+        .getSingleResult();
+  }
+
+  @Override
+  public List<ProblemSimpleResponseDto> selectProblemsWithProblemNumber(String problemNumber, int pageNumber, int pageSize) {
+    int offset = (pageNumber - 1) * pageSize;
+
+    String jpql = "SELECT new com.koreii.algoduck.problem.dto.response.ProblemSimpleResponseDto(p) "
+        + "FROM Problem p "
+        + "WHERE p.problemNumber LIKE :problemNumber "
+        + "ORDER BY p.problemNumber";
+
+    return entityManager.createQuery(jpql, ProblemSimpleResponseDto.class)
+        .setParameter("problemNumber", "%" + problemNumber + "%")
+        .setFirstResult(offset)
+        .setMaxResults(pageSize)
+        .getResultList();
+  }
+
+  @Override
+  public long countProblemsWithTitle(String title) {
+    String jpql = "SELECT COUNT(p) "
+        + "FROM Problem p "
+        + "WHERE p.title LIKE :title ";
+
+    return entityManager.createQuery(jpql, Long.class).setParameter("title", "%" + title + "%")
+        .getSingleResult();
+  }
+
+  @Override
+  public List<ProblemSimpleResponseDto> selectProblemsWithTitle(String title, int pageNumber, int pageSize) {
+    int offset = (pageNumber - 1) * pageSize;
+
+    String jpql = "SELECT new com.koreii.algoduck.problem.dto.response.ProblemSimpleResponseDto(p) "
+        + "FROM Problem p "
+        + "WHERE p.title LIKE :title "
+        + "ORDER BY p.problemNumber";
+
+    return entityManager.createQuery(jpql, ProblemSimpleResponseDto.class)
+        .setParameter("title", "%" + title + "%")
+        .setFirstResult(offset)
+        .setMaxResults(pageSize)
+        .getResultList();
+  }
+
+  @Override
+  public long countProblemsWithDifficulty(List<Integer> difficulties) {
+    if (difficulties.isEmpty()) {
+      return countAllProblems();
+    }
+
+    String jpql = "SELECT COUNT(p) "
+        + "FROM Problem p "
+        + "WHERE p.difficulty IN :difficulties";
+
+    return entityManager.createQuery(jpql, Long.class)
+        .setParameter("difficulties", difficulties)
+        .getSingleResult();
+  }
+
+  @Override
+  public List<ProblemSimpleResponseDto> selectProblemsWithDifficulty(List<Integer> difficulties, int pageNumber, int pageSize) {
+    if (difficulties.isEmpty()) {
+      return selectAllProblems(pageNumber, pageSize);
+    }
+
+    int offset = (pageNumber - 1) * pageSize;
+
+    String jpql = "SELECT new com.koreii.algoduck.problem.dto.response.ProblemSimpleResponseDto(p) "
+        + "FROM Problem p "
+        + "WHERE p.difficulty IN :difficulties "
+        + "ORDER BY p.problemNumber";
+
+    return entityManager.createQuery(jpql, ProblemSimpleResponseDto.class)
+        .setParameter("difficulties", difficulties)
+        .setFirstResult(offset)
+        .setMaxResults(pageSize)
+        .getResultList();
   }
 }
