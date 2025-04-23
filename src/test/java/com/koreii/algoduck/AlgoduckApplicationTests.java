4a329991a8decdf15a64eb20f962e3fd161d9ef6
jwelyl <cork2586@naver.com>
2025-04-23 18:58:24 +0900

    [bugfix] Testcontainers 기반으로 MySQL 컨테이너 구성하여 기본 테스트 실행 가능하도록 설정

--- src/test/java/com/koreii/algoduck/AlgoduckApplicationTests.java
+++ src/test/java/com/koreii/algoduck/AlgoduckApplicationTests.java
@@ -2,12 +2,30 @@ package com.koreii.algoduck;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class AlgoduckApplicationTests {
+  @Container
+  static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
+      .withDatabaseName("testdb")
+      .withUsername("testuser")
+      .withPassword("testpass");
 
-	@Test
-	void contextLoads() {
-	}
+  @DynamicPropertySource
+  static void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", mysql::getJdbcUrl);
+    registry.add("spring.datasource.username", mysql::getUsername);
+    registry.add("spring.datasource.password", mysql::getPassword);
+  }
+
+  @Test
+  void contextLoads() {
+  }
 
 }
