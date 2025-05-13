88f231a54c731f058fb13c23dcb5a7b59252d52a
jwelyl <cork2586@naver.com>
2025-05-13 22:50:54 +0900

    [feat] 채점에 필요한 Java 언어, 버전 정보 DB 추가
    - create 시 DB에 삽입되도록 init-db.sql 수정
    - @RequiredArgsConstructor 생성자주입으로 리팩토링

--- src/main/java/com/koreii/algoduck/data/DataInitializer.java
+++ src/main/java/com/koreii/algoduck/data/DataInitializer.java
@@ -2,6 +2,7 @@ package com.koreii.algoduck.data;
 
 import jakarta.annotation.PostConstruct;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
@@ -11,12 +12,10 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 
 @Component
+@RequiredArgsConstructor
 public class DataInitializer {
-  @Autowired
-  private Environment env;
-
-  @Autowired
-  private DataSource dataSource;
+  private final Environment env;
+  private final DataSource dataSource;
 
   @PostConstruct
   public void init() throws Exception {
@@ -25,8 +24,8 @@ public class DataInitializer {
     if ("create".equalsIgnoreCase(ddlAuto)) {
       try (Connection conn = dataSource.getConnection()) {
         // classpath:init-data.sql 실행
-        ScriptUtils.executeSqlScript(conn, new org.springframework.core.io.ClassPathResource("init-problems.sql"));
-        System.out.println("init-problems.sql executed!");
+        ScriptUtils.executeSqlScript(conn, new org.springframework.core.io.ClassPathResource("init-db.sql"));
+        System.out.println("init-db.sql executed!");
       }
     }
   }
