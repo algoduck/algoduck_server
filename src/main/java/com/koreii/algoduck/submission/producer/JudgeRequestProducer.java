6f3f667145a33d4e101ee9acb4d9a16ff5527dbf
jwelyl <cork2586@naver.com>
2025-06-17 21:11:36 +0900

    [feat] RabbitMQ 채점 요청 전송을 위한 기본 설정 및 구성 클래스 추가
    - spring-boot-starter-amqp 의존성 추가
    - JudgeRequestMessage 클래스 정의 (MQ 전송용 메시지 모델)
    - JudgeRequestProducer 컴포넌트 생성 (RabbitMQ 메시지 전송 로직)
    - RabbitMQConfig 설정 추가 (Jackson 기반 JSON 직렬화 설정 포함)
    - SubmissionServiceImpl 내부 로직에 MQ 연동을 위한 주석 추가 (아직 전송 로직은 미반영)

---
+++ src/main/java/com/koreii/algoduck/submission/producer/JudgeRequestProducer.java
@@ -0,0 +1,19 @@
+package com.koreii.algoduck.submission.producer;
+
+import com.koreii.algoduck.submission.message.request.JudgeRequestMessage;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+
+@RequiredArgsConstructor
+public class JudgeRequestProducer {
+  private final RabbitTemplate rabbitTemplate;
+
+  @Value("${spring.rabbitmq.queue.name}")
+  private String queueName;
+
+  public void sendJudgeRequest(JudgeRequestMessage message) {
+    rabbitTemplate.convertAndSend(queueName, message);
+  }
+}
