91879e3a197acaad4ea26ba87630a9f6b11e974b
jwelyl <cork2586@naver.com>
2025-06-22 22:51:23 +0900

    [test] RabbitMQ 자동 설정 제외 및 MQ Bean Mock 처리로 contextLoads 테스트 안정화
    - test profile에서 RabbitAutoConfiguration을 exclude하여 MQ 연결 시도 차단
    - TestMockConfig에서 RabbitTemplate과 AmqpAdmin을 mock으로 등록
    - Mockito.mockingDetails()를 통한 mock 확인 테스트 추가
    - contextLoads 테스트 시 MQ 연결 실패로 인한 CI 실패 방지

--- src/test/java/com/koreii/algoduck/config/TestMockConfig.java
+++ src/test/java/com/koreii/algoduck/config/TestMockConfig.java
@@ -5,12 +5,10 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 
 import static org.mockito.Mockito.mock;
 
 @TestConfiguration
-@Profile("test")
 public class TestMockConfig {
 
   @Bean
